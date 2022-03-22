99d7bbbfda89daa13aae74514f62e827879c8e1c
superhuman <superhuman@eduroam-193-157-183-142.wlan.uio.no>
2022-03-22 09:55:23 +0100

    s

--- policy_based/goexplore_py/dt_models.py
+++ policy_based/goexplore_py/dt_models.py
@@ -226,17 +226,15 @@ class GPT(object):
                 action_embeddings = tf.reshape(action_embeddings, (nenv, nsteps, n_embed))
                 # (batch, block_size, n_embd)
 
-                token_embeddings = np.zeros((nenv, nsteps*3 - int(test_mode), n_embed), dtype=np.float32).tolist()
-                token_embeddings[:,::3,:] = goal_embeddings
-                token_embeddings[:,1::3,:] = input_embeddings
-                token_embeddings[:,2::3,:] = action_embeddings[:,-nsteps + int(test_mode):,:]
+                token_embeddings = tf.reshape(tf.stack([goal_embeddings, input_embeddings, action_embeddings[:,-nsteps + int(test_mode):,:]], axis=1), (nenv, nsteps*3 - int(test_mode), n_embed))
+                # token_embeddings[:,::3,:] = goal_embeddings
+                # token_embeddings[:,1::3,:] = input_embeddings
+                # token_embeddings[:,2::3,:] = action_embeddings[:,-nsteps + int(test_mode):,:]
             else: # only happens at very first timestep of evaluation
                 # goal_embeddings =  tf.math.tanh(fc(goal,'goal_embed2', nout=n_embd, init_scale=0.02))
-                token_embeddings = np.zeros((nenv, nsteps*2, n_embed), dtype=np.float32)
-                token_embeddings[:,::2,:] = goal_embeddings # really just [:,0,:]
-                token_embeddings[:,1::2,:] = input_embeddings # really just [:,1,:]
-
-            token_embeddings = tf.convert_to_tensor(token_embeddings, np.float32)
+                token_embeddings = tf.reshape(tf.stack([goal_embeddings, input_embeddings], axis=-1), (nenv, nsteps*2, n_embed))
+                # token_embeddings[:,::2,:] = goal_embeddings # really just [:,0,:]
+                # token_embeddings[:,1::2,:] = input_embeddings # really just [:,1,:]
             all_global_pos_emb = tf.repeat(global_pos_emb, nenv, axis=0) # batch_size, traj_length, n_embd
 
             position_embeddings = torch_gather(all_global_pos_emb, tf.repeat(reshape(timesteps, (nenv, nsteps, 1)), n_embed, axis=-1), gather_axis=1) + pos_emb[:, :token_embeddings.shape.as_list()[1], :]
@@ -410,3 +408,8 @@ class Model(object):
     def train(self, lr, obs, goals, timesteps, masks, actions, valids, increase_ent):
         td_map = {self.LR: lr, self.train_model.X: obs,  self.train_model.goal: goals, self.T: timesteps, self.A: actions, self.train_model.A: actions, self.train_model.E: increase_ent}
         return self.sess.run(self.loss_requested, feed_dict=td_map)[:-1]
+
+# x = tf.zeros((8,4,2))
+# y = tf.ones((8,4,2))
+# z = tf.reshape(tf.stack([x, y], axis=1), (8,8,2))
+# print(z)
